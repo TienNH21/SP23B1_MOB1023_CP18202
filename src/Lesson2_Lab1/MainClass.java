5f03496f5491c1f269e076154b9fb0708129db8d
HoangTien339 <tiennh21@fpt.edu.vn>
2023-01-05 13:48:23 +0700

    Lesson 2: Lab 1

---
+++ src/Lesson2_Lab1/MainClass.java
@@ -0,0 +1,15 @@
+package Lesson2_Lab1;
+
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap ten SP:");
+        String name = sc.nextLine();
+        System.out.println("Nhap gia SP:");
+        double price = Double.parseDouble(sc.nextLine());
+        
+        Product p = new Product(name, price);
+    }
+}
---
+++ src/Lesson2_Lab1/ProductService.java
@@ -0,0 +1,28 @@
+package Lesson2_Lab1;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ProductService implements IProductService {
+    private ArrayList<Product> dssp = new ArrayList<>();
+
+    @Override
+    public void insert(Product p) {
+        this.dssp.add(p);
+    }
+
+    @Override
+    public void update(int viTri, Product p) {
+        this.dssp.set(viTri, p);
+    }
+
+    @Override
+    public void delete(int viTri) {
+        this.dssp.remove(viTri);
+    }
+
+    @Override
+    public ArrayList<Product> select() {
+        return this.dssp;
+    }
+}
